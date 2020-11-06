f7ad1ff5ce9bdaadb5f39fbd6f643bfd4d246719
keisuke0831 <irohasu85@gmail.com>
2020-11-06 22:21:05 +0900

    商品一覧表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,15 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  
+
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.order('created_at DESC')
     
   end
 
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -20,9 +20,11 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def item_params
-    params.require(:item).permit(:name,:description,:category_id,:item_condition_id,:prefecture_id,:ship_date_id,:shipping_cost_id,:image,:price,).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :item_condition_id, :prefecture_id, :ship_date_id, :shipping_cost_id, :image, :price).merge(user_id: current_user.id)
   end
+
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,22 +7,19 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :ship_date
   belongs_to :shipping_cost
-  has_one_attached :image 
+  has_one_attached :image
   with_options presence: true do
     validates :name
     validates :description
-    validates :image 
-    validates :price, numericality: {  greater_than_or_equal_to: 300, less_than: 10000000} , format:{ with: /\A[0-9]+\z/, message: 'は半角（数値）での入力が必須です' } 
-  
-    
-    with_options numericality: { other_than: 1 }  do 
+    validates :image
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than: 10_000_000 }, format: { with: /\A[0-9]+\z/, message: 'は半角（数値）での入力が必須です' }
+
+    with_options numericality: { other_than: 1 } do
       validates :category_id
       validates :item_condition_id
       validates :prefecture_id
       validates :ship_date_id
       validates :shipping_cost_id
     end
-
   end
-  
 end
